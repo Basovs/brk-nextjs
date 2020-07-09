908be293fa0425537e977e7a49f629d369529803
Janis Basis Basovs PC <janis_basis@hotmail.com>
2020-07-09 13:43:34 +0300

    and now?

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,13 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import Hero from "../components/Hero";
 import InfoBar from "../components/InfoBar";
-import PhilosophySection from "../components/Philosophy";
+import PhilosophySection from "../components/philosophy";
 
 const Index = () => {
   return (
     <>
       <Head>
-        <title>BRKs</title>
+        <title>BRK</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
