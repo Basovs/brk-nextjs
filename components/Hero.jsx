60087993e592c5733c0f50c5c9654aba527a9b67
Janis Basis Basovs PC <janis_basis@hotmail.com>
2020-07-09 13:35:37 +0300

    vercel fail -wtf?

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
           hac habitasse platea dictumst. In eu nisi molestie, rhoncus ante eget,
           ornare nunc. Suspendisse cursus dapibus ligula ut placerat. Curabitur
           sollicitudin velit sit amet risus pellentesque gravida. In hac
-          habitasse platea dictumst. In eu nisi molestie, rhoncus ante eget,
+          habitasse platea dsictumst. In eu nisi molestie, rhoncus ante eget,
           ornare nunc. Suspendisse cursus dapibus ligula ut placerat. In hac
           habitasse platea dictumst.
         </p>
