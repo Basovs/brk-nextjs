d7bb923d7d651c0b5a12914fceb85e4fe7a08936
Janis Basis Basovs <janis_basis@hotmail.com>
2020-07-24 08:48:47 +0300

    testing overflow

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import Layout from "../components/Layout";
-import Hero from "../components/Hero";
-import InfoBar from "../components/InfoBar";
-import PhilosophySection from "../components/philosophy";
-import OfferSection from "../components/OfferSection";
-import FindUs from "../components/FindUs";
-import Partners from "../components/Partners";
-import Footer from "../components/Footer";
-import HumburgerNav from "../components/HumburgerNav";
+import Head from "next/head"
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import InfoBar from "../components/InfoBar"
+import PhilosophySection from "../components/philosophy"
+import OfferSection from "../components/OfferSection"
+import FindUs from "../components/FindUs"
+import Partners from "../components/Partners"
+import Footer from "../components/Footer"
+import HumburgerNav from "../components/HumburgerNav"
 
 const Index = () => {
   return (
@@ -15,10 +15,7 @@ const Index = () => {
       <Head>
         <title>BRK</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Description about the company for SEO"
-        ></meta>
+        <meta name="description" content="Description about the company for SEO"></meta>
       </Head>
 
       <Layout>
@@ -32,12 +29,29 @@ const Index = () => {
         <Footer />
       </Layout>
       <style jsx global>{`
+        html,
+        body,
+        h1,
+        h2,
+        h3,
+        h4,
+        ul {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
         html {
+          background: #fff;
           overflow-x: hidden;
+        }
+
+        * {
+          box-sizing: border-box;
         }
       `}</style>
     </>
-  );
-};
+  )
+}
 
-export default Index;
+export default Index
